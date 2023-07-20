17bed862f56da493f0cf97c1f82245d9222ba1af
kelvincsantos <54078315+kelvincsantos@users.noreply.github.com>
2023-07-20 01:14:32 -0300

    .

--- Nucleo.Forms/Comum/Conversor.cs
+++ Nucleo.Forms/Comum/Conversor.cs
@@ -8,7 +8,18 @@ namespace Nucleo.Forms.Comum
     {
         public static bool EnterToTab(char e)
         {
-            return true;
+            if (e == (char)13)
+            {
+                SendKeys.Send("{tab}");
+                return true;
+            }
+            else if (e == (char)39)
+            {
+                SendKeys.Send("{u}");
+                SendKeys.Send("{BackSpace}");
+                return true;
+            }
+            return false;
         }
     }
 }
--- Nucleo.Forms/Program.cs
+++ Nucleo.Forms/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,10 +15,65 @@ namespace Nucleo.Forms
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MenuPrincipal());
+
+            bool createdNew = true;
+
+            using (Mutex mutex = new Mutex(true, "FrenteCaixaBoxPDV", out createdNew))
+            {
+                if (createdNew)
+                {
+                    //Environment = new Core.Application.Environment();
+
+                    //Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+
+                    Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
+                    AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+                    //Environment.Main = new Main();
+                    //Environment.Main.Run();
+                }
+                else
+                {
+                    //State.Exit();
+                }
+            }
+
+            //Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new MenuPrincipal());
+        }
+
+        /*
+         [STAThread]
+        static void Main()
+        {
+            
+        }
+
+        
+         */
+
+        static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            // Log the exception, display it, etc
+
+            //Debug.WriteLine("Application_ThreadException");
+            //Debug.WriteLine(e.Exception.Message);
+
+            //MessageBox.Show(e.Exception.Message);
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            // Log the exception, display it, etc
+
+            //Debug.WriteLine("CurrentDomain_UnhandledException");
+            //Debug.WriteLine((e.ExceptionObject as Exception).Message);
+
+            //MessageBox.Show((e.ExceptionObject as Exception).Message);
         }
     }
 }
