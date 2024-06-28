a544f7b02a2e9a41ebd18e7c08e63c01e7b6054b
kelvincsantos <54078315+kelvincsantos@users.noreply.github.com>
2024-06-28 20:41:04 -0300

    .

---
+++ Base/Comum/Processos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleo.Base.Comum
+{
+    public static class Processos
+    {
+        public static string Executar(string driveExePath, string args)
+        {
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = driveExePath;
+                process.StartInfo.Arguments = args;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.Start();
+
+                string output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+
+                return output;
+            }
+        }
+    }
+}
--- Nucleo.Data/obj/Debug/net8.0/Nucleo.Data.AssemblyInfo.cs
+++ Nucleo.Data/obj/Debug/net8.0/Nucleo.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nucleo.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dc7157eb79639833f5d689fbd1eab9237c51ddf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ac24c3d1001de6af5a2345cd222ca96e6055f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nucleo.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nucleo.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
