a88c3576d86346119f467f13ebf29719544c9a8e
kelvincsantos <54078315+kelvincsantos@users.noreply.github.com>
2025-06-26 23:33:43 -0300

    .

---
+++ API/Nucleo.API.Core/Externas/Brapi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleo.API.Core.Externas
+{
+    public class Brapi
+    {
+
+        public Brapi() { }
+
+        public Retornos.Brapi.BuscarTicker BuscarTicker(string Ticker)
+        {
+            return new Retornos.Brapi.BuscarTicker();
+        }
+    }
+}
---
+++ API/Nucleo.API.Core/Externas/Retornos/Brapi/BuscarTicker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleo.API.Core.Externas.Retornos.Brapi
+{
+    internal class BuscarTicker
+    {
+    }
+}
